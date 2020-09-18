8c74c764a75cd702d1bbadbf1a445d1c850e5517
Enjha <proxzy-yann@hotmail.fr>
2020-09-18 18:28:27 +0200

    Programme avancé

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("aavl.fxml")));
+        
+       
+        primaryStage.getIcons().add(new Image("avl.png"));
+        primaryStage.setScene(new Scene(root, 1000, 650));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
